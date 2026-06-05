6609afedbbbb3e2db9cfe51909fa6c533c51c8ae
Leo <leo.lebleis@gmail.com>
2026-06-05 19:40:07 +0100

    feat: CLI entry point with flag parsing and env var resolution

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"time"
 )
@@ -36,4 +38,42 @@ func run(host, dir string, cb Clipboard, up Uploader) error {
 	return nil
 }
 
-func main() {}
+func main() {
+	host := flag.String("host", "", "SSH host (overrides SCPCLIP_HOST env var)")
+	dir := flag.String("dir", "", "Remote directory (overrides SCPCLIP_DIR env var, default /tmp)")
+	showVersion := flag.Bool("version", false, "Print version and exit")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Println("scpclip", version)
+		return
+	}
+
+	if *host == "" {
+		*host = os.Getenv("SCPCLIP_HOST")
+	}
+	if *host == "" {
+		fmt.Fprintln(os.Stderr, "error: no host specified (use --host or set SCPCLIP_HOST)")
+		os.Exit(1)
+	}
+
+	if *dir == "" {
+		*dir = os.Getenv("SCPCLIP_DIR")
+	}
+	if *dir == "" {
+		*dir = "/tmp"
+	}
+
+	if _, err := exec.LookPath("ssh"); err != nil {
+		fmt.Fprintln(os.Stderr, "error: ssh not found on PATH")
+		os.Exit(1)
+	}
+
+	cb := &OSClipboard{}
+	up := NewSSHUploader()
+
+	if err := run(*host, *dir, cb, up); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
